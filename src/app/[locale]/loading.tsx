2689b2969444a336b455517a8a6c0a7bc377a24b
deerpark <deerpark@iMac.local>
2024-02-01 23:49:23 +0900

    chore: lint

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -4,7 +4,9 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import styles from '@/styles/styles.module.css'
 
 export default function Loading() {
-  return <div className={styles.container}>
-    <FontAwesomeIcon icon={faSpinnerThird} />
-  </div>
+  return (
+    <div className={styles.container}>
+      <FontAwesomeIcon icon={faSpinnerThird} />
+    </div>
+  )
 }
