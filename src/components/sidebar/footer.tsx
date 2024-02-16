67b2c74ee9d6b351ddd7b076b5275271fc093018
Yonn Kim <ji9@me.com>
2024-02-17 00:17:40 +0900

    fix: clerk css

--- src/components/sidebar/footer.tsx
+++ src/components/sidebar/footer.tsx
@@ -16,14 +16,14 @@ export default function SidebarFooter() {
   return (
     <div className="border-border/50 filter-blur sticky inset-x-0 bottom-0 z-10 flex w-full flex-col space-y-3 border-t pb-[calc(8px+env(safe-area-inset-bottom))] pl-5 pr-2 pt-2">
       <div className="flex w-full items-center justify-between space-x-1">
-        <FaUserTieIcon className="size-5 lg:size-4" />
         <SignedIn>
           <UserButton afterSignOutUrl="/" />
-          <span className="text-muted-foreground flex-1 px-2 text-left text-base/5 lg:text-sm/5">
+          <span className="text-muted-foreground flex-1 truncate px-2 py-3 text-left text-base/5 lg:text-sm/5">
             {t("welcome", { fullName: user?.fullName || "" })}
           </span>
         </SignedIn>
         <SignedOut>
+          <FaUserTieIcon className="size-5 lg:size-4" />
           <Link
             href="/sign-in"
             className={cn(
@@ -43,8 +43,8 @@ export default function SidebarFooter() {
           >
             {t("signup")}
           </Link>
+          <div className="flex-1" />
         </SignedOut>
-        <div className="flex-1" />
       </div>
     </div>
   )
